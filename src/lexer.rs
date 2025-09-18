90fb0744d133b1dec2c525449bc61ecf7a567b3d
banana <fotyek.robert63@gmail.com>
2025-09-18 11:42:42 +0200

    lex helpers start

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,9 +6,38 @@ use token::Token;
 
 pub struct Lexer {
     code: String,
-    start: u32,
-    current: u32,
-    line: u32,
+    start: usize,
+    current: usize,
+    line: usize,
 }
 
-pub fn lex(code: &str) -> Result<(Vec<Token>, Vec<FrontendError>)> {}
+impl Lexer {
+    pub fn new(code: &str) -> Self {
+        Lexer {
+            code: String::from(code),
+            start: 0,
+            current: 0,
+            line: 1,
+        }
+    }
+
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        self.code
+            .chars()
+            .nth(self.current - 1)
+            .expect("lexer indexed out of code string!")
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.code.len()
+    }
+}
+
+pub fn lex(code: &str) -> Result<(Vec<Token>, Vec<FrontendError>)> {
+    let tokens: Vec<Token> = vec![];
+    let ferrors: Vec<FrontendError> = vec![];
+    let lexer = Lexer::new(code);
+
+    Ok((tokens, ferrors))
+}
